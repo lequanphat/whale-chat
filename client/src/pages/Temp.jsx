6f923c7e8821d440263ef2e5768e9b63970727fb
lequanphat <lequanphat3579@gmail.com>
2023-11-24 23:59:24 +0700

    update page not found

--- client/src/pages/Temp.jsx
+++ client/src/pages/Temp.jsx
@@ -1,9 +1,9 @@
 import styled from 'styled-components';
 
-function Temp({title, content}) {
+function Temp({title, content, image}) {
     return <Container>
-        <div className='container'>
-            <img src='robot.gif' alt='logo'/>
+        <div className={`container`}>
+            <img src={image ? image : 'robot.gif'} alt='logo'/>
             <h1>{title ? title : 'Coming Soon'}</h1>
             <h2>{content ? content : 'Thanks for visit this site. See you soon!'}</h2>
         </div>
@@ -11,13 +11,14 @@ function Temp({title, content}) {
 }
 const Container = styled.div`
     height: 100%;
-    background-color: #131324;
     display: flex;
+    background-color:'#131324';
     align-items: center;
     justify-content: center;
     text-align: center;
     img{
         width: 20rem;
+        margin-bottom: 1rem;
     }
     h1{
         color: #1abc9c;
