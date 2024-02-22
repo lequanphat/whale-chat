7987c3a7c01525b5eb3115da5063ce504b4caeaa
lequanphat <lequanphat3579@gmail.com>
2024-02-22 20:47:39 +0700

    on/off microphone, camera while video call

--- client/src/components/calls/VideoCalls.tsx
+++ client/src/components/calls/VideoCalls.tsx
@@ -1,6 +1,14 @@
 import { Avatar, Box, Dialog, DialogTitle, IconButton, Stack, Typography, useTheme } from '@mui/material';
 import Peer from 'peerjs';
-import { IoCallOutline, IoCloseOutline } from 'react-icons/io5';
+import {
+  IoCallOutline,
+  IoCloseOutline,
+  IoScanOutline,
+  IoVideocamOutline,
+  IoVideocamOffOutline,
+  IoMicOutline,
+  IoMicOffOutline,
+} from 'react-icons/io5';
 import Transition from '../dialog/Transition';
 import { useEffect, useRef, useState } from 'react';
 import { useSocket } from '../../hooks/useSocket';
@@ -13,16 +21,23 @@ const VideoCalls = ({ open }: { open: boolean }) => {
   // stream
   const [stream, setStream] = useState(null);
   const [remoteStream, setRemoteStream] = useState(null);
+
+  // state
+  const [isFullScreen, setIsFullScreen] = useState<boolean>(false);
+  const [isMicrophoneOn, setIsMicrophoneOn] = useState<boolean>(true);
+  const [isCameraOn, setIsCameraOn] = useState<boolean>(true);
   // peer connection
   const [myPeer, setMyPeer] = useState<Peer>();
   // ref
   const videoRef = useRef(null);
   const remoteVideoRef = useRef(null);
-  //
+  // emit
   const { emitVideoCall, emitInterruptVideoCall, emitAcceptVideoCall, emitRefuseVideoCall } = useSocket();
+  // store state
   const { call, currentContact } = useSelector((state: stateType) => state.chat);
   const { id } = useSelector((state: stateType) => state.auth);
 
+  // init effect
   useEffect(() => {
     handleInitPeer();
   }, []);
@@ -133,18 +148,52 @@ const VideoCalls = ({ open }: { open: boolean }) => {
     dispatch(refuseIncomingCall());
   };
 
+  // handle toggle full screen
+  const handleToggeFullScreen = () => {
+    setIsFullScreen((pre) => !pre);
+  };
+  // handle toggle microphone
+  const toggleMicrophone = () => {
+    if (stream) {
+      const audioTracks = stream.getAudioTracks();
+      audioTracks.forEach((track) => {
+        track.enabled = !track.enabled;
+      });
+      setIsMicrophoneOn(!isMicrophoneOn);
+    }
+  };
+  // handle toggle camera
+  const toggleCamera = () => {
+    if (stream) {
+      const videoTracks = stream.getVideoTracks();
+      videoTracks.forEach((track) => {
+        track.enabled = !track.enabled;
+      });
+      setIsCameraOn(!isCameraOn);
+    }
+  };
+
   // render
   return (
     <Dialog
       open={open}
       keepMounted
-      //   fullScreen
+      fullScreen={isFullScreen}
       fullWidth
       onClose={handleOffStream}
       aria-describedby="alert-dialog-slide-description"
       TransitionComponent={Transition}
     >
-      <DialogTitle>Video Call</DialogTitle>
+      <DialogTitle>
+        <Stack direction="row" alignItems="center" justifyContent="space-between">
+          <Typography variant="h6">Video Call</Typography>
+          {call.calling && (
+            <IconButton onClick={handleToggeFullScreen}>
+              <IoScanOutline size={22} />
+            </IconButton>
+          )}
+        </Stack>
+      </DialogTitle>
       {call.owner !== id && !call.accepted ? (
         <Stack direction="column" p={2}>
           <Stack direction="column" alignItems="center" justifyContent="center">
@@ -153,7 +202,7 @@ const VideoCalls = ({ open }: { open: boolean }) => {
             </Box>
             <Typography variant="h6">{call.contact?.displayName}</Typography>
           </Stack>
-          <Stack direction="row" alignItems="center" justifyContent="space-around" pt={2}>
+          <Stack direction="row" alignItems="center" justifyContent="center" pt={2} gap={16}>
             <Box sx={{ bgcolor: theme.palette.success.main, borderRadius: '50%' }}>
               <IconButton onClick={handleAcceptCall} sx={{ color: '#fff' }}>
                 <IoCallOutline size={22} />
@@ -186,7 +235,12 @@ const VideoCalls = ({ open }: { open: boolean }) => {
 
             {call.pending && (
               <>
-                <video ref={videoRef} autoPlay playsInline style={{ width: '50%', height: 'auto' }}></video>
+                <video
+                  ref={videoRef}
+                  autoPlay
+                  playsInline
+                  style={{ width: '50%', height: 'auto', borderRadius: '5%' }}
+                ></video>
                 <Stack flex={1} direction="column" alignItems="center" justifyContent="center">
                   <Avatar src={call.contact.avatar} />
                   <Typography variant="h6">{call.contact.displayName}</Typography>
@@ -196,33 +250,53 @@ const VideoCalls = ({ open }: { open: boolean }) => {
             )}
             {call.calling && (
               <>
-                <video ref={videoRef} autoPlay playsInline style={{ width: '45%', height: 'auto' }}></video>
-                <video ref={remoteVideoRef} autoPlay playsInline style={{ width: '45%', height: 'auto' }}></video>
+                <video
+                  ref={videoRef}
+                  autoPlay
+                  playsInline
+                  style={{ width: '48%', height: 'auto', borderRadius: '5%' }}
+                ></video>
+                <video
+                  ref={remoteVideoRef}
+                  autoPlay
+                  playsInline
+                  style={{ width: '48%', height: 'auto', borderRadius: '5%' }}
+                ></video>
               </>
             )}
           </Stack>
-          <Stack direction="row" justifyContent="space-around" p={2} pt={4}>
-            {!call.pending && !call.calling ? (
-              <>
-                <Box sx={{ bgcolor: theme.palette.success.main, borderRadius: '50%' }}>
-                  <IconButton onClick={handleReCall} sx={{ color: '#fff' }}>
-                    <IoCallOutline size={26} />
-                  </IconButton>
-                </Box>
-                <Box sx={{ bgcolor: theme.palette.error.main, borderRadius: '50%' }}>
-                  <IconButton onClick={handleCloseCall} sx={{ color: '#fff' }}>
-                    <IoCloseOutline size={26} />
-                  </IconButton>
-                </Box>
-              </>
-            ) : (
+          {!call.pending && !call.calling ? (
+            <Stack direction="row" justifyContent="center" p={2} pt={4} gap={16}>
+              <Box sx={{ bgcolor: theme.palette.success.main, borderRadius: '50%' }}>
+                <IconButton onClick={handleReCall} sx={{ color: '#fff' }}>
+                  <IoCallOutline size={26} />
+                </IconButton>
+              </Box>
+              <Box sx={{ bgcolor: theme.palette.error.main, borderRadius: '50%' }}>
+                <IconButton onClick={handleCloseCall} sx={{ color: '#fff' }}>
+                  <IoCloseOutline size={26} />
+                </IconButton>
+              </Box>
+            </Stack>
+          ) : (
+            <Stack direction="row" justifyContent="center" p={2} pt={4} gap={3}>
+              <Box sx={{ bgcolor: '#ccc', borderRadius: '50%' }}>
+                <IconButton onClick={toggleMicrophone} sx={{ color: '#fff' }}>
+                  {isMicrophoneOn ? <IoMicOutline size={26} /> : <IoMicOffOutline size={26} />}
+                </IconButton>
+              </Box>
               <Box sx={{ bgcolor: theme.palette.error.main, borderRadius: '50%' }}>
                 <IconButton onClick={handleOffStream} sx={{ color: '#fff' }}>
                   <IoCallOutline size={26} />
                 </IconButton>
               </Box>
-            )}
-          </Stack>
+              <Box sx={{ bgcolor: '#ccc', borderRadius: '50%' }}>
+                <IconButton onClick={toggleCamera} sx={{ color: '#fff' }}>
+                  {isCameraOn ? <IoVideocamOutline size={26} /> : <IoVideocamOffOutline size={26} />}
+                </IconButton>
+              </Box>
+            </Stack>
+          )}
         </Stack>
       )}
     </Dialog>
